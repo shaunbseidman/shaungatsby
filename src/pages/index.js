e93fd4d02d14eb3f5debc82a9c841ed163aa8fcf
Shaun Seidman <shaun.seidman@MBP-300357.local>
2020-05-28 12:37:36 -0600

    styles getting clobbered

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,14 +75,10 @@ class IndexPage extends React.Component{
               <span className="loopedText">attempting to vacuum more</span>
             </TextLoop>
           </p>
-          <div className="HeroLink">
-            <div
-            onClick={e => {
+          <span className="HeroLink"
+          onClick={e => {
              this.showForm(e);
-            }}>
-            Let's Get In Touch
-            </div>
-          </div>
+          }}>Let's Get In Touch</span>
           <Modal onClose={this.showForm} show={this.state.show}>
           <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field">
             <h4>I'll get back to you ASAP (I mean it)</h4>
