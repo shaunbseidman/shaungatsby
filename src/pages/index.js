e6fb7457b830171cda885f4a6aabb12e93738fad
Shaun Seidman <shaun.seidman@MBP-300357.local>
2020-05-28 11:31:01 -0600

    form still broken

--- src/pages/index.js
+++ src/pages/index.js
@@ -80,23 +80,12 @@ class IndexPage extends React.Component{
              this.showForm(e);
           }}>Let's Chat!</div>
           <form className="emailForm" name="contact" method="POST" data-netlify="true">
-  <p>
-    <label>Your Name: <input type="text" name="name" /></label>   
-  </p>
-  <p>
-    <label>Your Email: <input type="email" name="email" /></label>
-  </p>
-  <p>
-    <label>Your Role: <select name="role[]" multiple>
-      <option value="leader">Leader</option>
-      <option value="follower">Follower</option>
-    </select></label>
-  </p>
-  <p>
-    <label>Message: <textarea name="message"></textarea></label>
-  </p>
-  <p>
-    <button type="submit">Send</button>
+            <label>Your Name: <input type="text" name="name" placeholder="Your Name" /></label>
+            <p>
+              <label>Message: <textarea name="message"></textarea></label>
+            </p>
+            <p>
+          <button>Send</button>
   </p>
 </form>
           <Modal onClose={this.showForm} show={this.state.show}>
